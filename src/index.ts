600c21efb992ce967b40e2a1558c153af46273e5
Tyler Liu <tyler4long@gmail.com>
2024-01-14 09:32:33 -0800

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log(`Hello ${process.env.NAME}!`);
+import { getHistoricalRates } from 'dukascopy-node';
+
+const main = async () => {
+  try {
+    const data = await getHistoricalRates({
+      instrument: 'aaplususd',
+      dates: {
+        from: new Date('2024-01-01'),
+        to: new Date('2024-01-15'),
+      },
+      timeframe: 'd1',
+      format: 'json',
+    });
+
+    console.log(data);
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+main();
